d6e5dece9b318a269d28246551a6f79c8985b74e
Aleksandr Lvov <lvov.as@yandex.ru>
2021-03-04 16:50:58 +0500

    Initial commit

---
+++ src/main/java/app/bot/MathExpressionSolverTelegramBot.java
@@ -0,0 +1,40 @@
+package app.bot;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class MathExpressionSolverTelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "name";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "token";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+
+            SendMessage sm = new SendMessage();
+            sm.setChatId(chatId);
+            sm.setText(message);
+
+            try {
+                execute(sm);
+            } catch (TelegramApiException e) {
+                //todo add logging to the project.
+                e.printStackTrace();
+            }
+        }
+    }
+}
